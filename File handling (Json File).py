88a772065ee8d131c89da41acb20e0c303e53412
riyashinde21 <riyashinde21122005@gmail.com>
2026-04-17 14:58:00 +0530

    Add files via upload

---
+++ File handling (Json File).py
@@ -0,0 +1,9 @@
+
+
+[
+    {"name":"Riya","mark":"95","age":25},
+     {"name":"Kritika","mark":"65","age":19},
+      {"name":"Tanvi","mark":"80","age":18},
+       {"name":"Roy","mark":"79","age":19}
+    
+]
